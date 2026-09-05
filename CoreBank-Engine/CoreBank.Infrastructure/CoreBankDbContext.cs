af816e1777650c6c11bcd800376eec4e270b2f92
quyn03z <quyenvn03@gmail.com>
2026-09-05 13:50:16 +0700

    add project corebank

--- CoreBank-Engine/CoreBank.Infrastructure/CoreBankDbContext.cs
+++ CoreBank-Engine/CoreBank.Infrastructure/CoreBankDbContext.cs
@@ -33,8 +33,9 @@ public partial class CoreBankDbContext : DbContext
     public virtual DbSet<UserDevice> UserDevices { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=CoreBank_Engine;User Id=sa;Password=123456;TrustServerCertificate=True;");
+    {
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ CoreBank-Engine/CoreBank.Infrastructure/DataAccessDependencyInjection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreBank.Infrastructure
+{
+    public class DataAccessDependencyInjection
+    {
+    }
+}
